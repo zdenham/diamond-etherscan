8cd1a07744a58d4cac8f651a0070914d8a8ce8be
zdenham <zdenham1@gmail.com>
2022-11-11 16:45:22 -0800

    moving to main

--- utils/fetchFacetsABI.js
+++ utils/fetchFacetsABI.js
@@ -1,3 +1,13 @@
-const fetchFacetsABI = async (diamondAddress) => {
-  const fullUrl = `${apiUrl}?module=contract&action=getsourcecode&address=${diamondAddress}&apikey=${API_KEY}`;
+require('dotenv').config();
+
+const getEtherscanApiKey = (network) => {
+  const key = process.env[`${network.toUpperCase()}_ETHERSCAN_API_KEY`]
+  console.log(key)
+  return key ? key : process.env.ETHERSCAN_API_KEY
+}
+
+export const fetchFacetsABI = async (diamondAddress, network) => {
+  const apiUrl = NETWORKS[network].explorerApiUrl;
+  const fullUrl = `${apiUrl}?module=contract&action=getsourcecode&address=${diamondAddress}&apikey=${process.env.API_KEY}`;
+
 }
