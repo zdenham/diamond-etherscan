64b0b45f2680b7c6da3108a9b8d90d1010669b91
zdenham <zdenham1@gmail.com>
2022-11-13 16:59:22 -0800

    starting web page

---
+++ web/src/components/Lottie.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import lottie from 'lottie-web';
+	import { onMount } from 'svelte';
+
+	export let loop: boolean = false;
+	export let autoplay: boolean = true;
+	export let path: string;
+	let el: HTMLDivElement;
+
+	onMount(() => {
+		lottie.loadAnimation({
+			container: el, // the dom element that will contain the animation
+			renderer: 'svg',
+			loop,
+			autoplay,
+			path
+		});
+	});
+</script>
+
+<div class="wrapper">
+	<div bind:this={el} />
+</div>
+
+<style>
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+  import Lottie from '../components/Lottie.svelte';
+
+  let diamondAddress: string = '';
+  let loading: boolean = false;
+
+</script>
+
+<div class="wrapper">
+  <h1>Diamond Etherscan</h1>
+  <a
+    class="github-button"
+    href="https://github.com/zdenham/redis-ipfs"
+    data-size="large"
+    data-show-count="true"
+    aria-label="Star zdenham/redis-ipfs on GitHub"
+    target="_blank"
+    >
+    Github ⭐️
+  </a>
+  {#if loading}
+  <div class="preload">
+    <Lottie path={'/diamond.json'} loop />
+  </div>
+  {/if}
+
+
+</div>
+
+<style>
+  .preload {
+    width: 300px;
+    height: 300px;
+  }
+
+  .wrapper {
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  :global(body) {
+    margin: 0;
+    overflow-x: hidden;
+    font-family: 'NunitoSans';
+  }
+
+  @font-face {
+    font-family: NunitoSans;
+    src: url('/NunitoSans-SemiBold.ttf');
+  }
+
+  
+</style>
+
+
+
+
+
